b7dc913859fb25b26e2c387265cf250c8d8541d6
shiheric <f14051172@gs.ncku.edu.tw>
2024-09-23 19:59:32 +0800

    Update screen in another thread

--- main.cc
+++ main.cc
@@ -1,15 +1,37 @@
 #include <SDL2/SDL_timer.h>
 #include <stdio.h>
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
 #include "core/include/game_statemachine.h"
 #include "core/include/sdl_handler.h"
 #include "core/include/sprite_handler.h"
+
+std::condition_variable cv;
+std::mutex m;
+std::unique_lock<std::mutex> lock(m);
+bool is_process_alive = true;
+
+void update_screen() {
+    // This function is running in update_thread.
+    while (is_process_alive) {
+        cv.wait(lock);
+        printf("Update screen...\n");
+        sdl_handler::GetInstance().update_screen();
+    }
+    printf("Teardown update_thread...\n");
+}
 
 int main(int argc, char* argv[]) {
     sdl_handler::GetInstance().initialize();
     SDL_Renderer* surface = sdl_handler::GetInstance().get_surface();
 
     sprite_handler::GetInstance().initialize();
+
+    // Create a thread for updating screen
+    std::thread update_thread(update_screen);
 
     printf("Entering main loop...\n");
     int close = 0;
@@ -20,7 +42,7 @@ int main(int argc, char* argv[]) {
     game_sm.start();
     while (!close) {
         // 60 FPS
-        if (SDL_GetTicks() - start_time < 1000/60.0) {
+        if (SDL_GetTicks() - start_time < 1000 / 60.0) {
             continue;
         }
         start_time = SDL_GetTicks();
@@ -48,8 +70,12 @@ int main(int argc, char* argv[]) {
 
         game_sm.process();
 
-        sdl_handler::GetInstance().update_screen();
+        cv.notify_all();  // Notify update_thread to process.
     }
+
+    // Teardown threads
+    is_process_alive = false;
+    update_thread.join();
 
     sdl_handler::GetInstance().teardown();
     sprite_handler::GetInstance().teardown();
